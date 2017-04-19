f6e65199f3ae0b7121be9395c295c1231f794eb0
hha046 <harald.s.hanssen@hnikt.no>
2017-04-19 12:40:27 +0200

    Added a template server project based on nuget packages instead of direct references.
    The nuget packages Bonfire, Flare and Spark.Engine STU3 versions are available from nuget.org

--- src/FhirStarter.Flare.STU3/Properties/AssemblyInfo.cs
+++ src/FhirStarter.Flare.STU3/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FhirStarter.Flare.STU3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Contains the necessary classes to create a FHIR server. Both the setup classes and FHIR controller is here.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Helse Nord IKT")]
 [assembly: AssemblyProduct("FhirStarter.Flare.STU3")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
