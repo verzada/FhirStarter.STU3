292330afe04db61f24ba218af5b85df4185c7ea6
Jon-Vidar Schneider <Jon-Vidar.Schneider@hn-ikt.no>
2017-09-12 10:18:47 +0200

    Added validation on Create

--- src/FhirStarter.Flare.STU3/Controllers/FhirController.cs
+++ src/FhirStarter.Flare.STU3/Controllers/FhirController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -74,19 +75,26 @@ namespace FhirStarter.Flare.STU3.Controllers
         [HttpGet, Route("StructureDefinition/{id}")]
         public HttpResponseMessage GetStructureDefinition(string id)
         {
-            var structureDefinitionNames = _handler.GetStructureDefinitionNames(_fhirServices);
-            if (!structureDefinitionNames.Contains(id))
-            {
-                throw new FhirOperationException($"{nameof(StructureDefinition)} {id} not found", HttpStatusCode.InternalServerError);
-            }
-            var structureDefinitions = _fhirStructureDefinitionService.GetStructureDefinitions();
-            var structureDefinition = structureDefinitions.FirstOrDefault(definition => definition.Name.Equals(id));
+            var structureDefinition = Load(id);
             if (structureDefinition != null)
             {
                 return SendResponse(structureDefinition);
             }
             throw new FhirOperationException($"{nameof(StructureDefinition)} for {id} not found", HttpStatusCode.InternalServerError);
             
+        }
+
+        private StructureDefinition Load(string id)
+        {
+            var structureDefinitionNames = _handler.GetStructureDefinitionNames(_fhirServices);
+            if (!structureDefinitionNames.Contains(id))
+            {
+                throw new FhirOperationException($"{nameof(StructureDefinition)} {id} not found",
+                    HttpStatusCode.InternalServerError);
+            }
+            var structureDefinitions = _fhirStructureDefinitionService.GetStructureDefinitions();
+            var structureDefinition = structureDefinitions.FirstOrDefault(definition => definition.Name.Equals(id));
+            return structureDefinition;
         }
 
         [HttpGet, Route("{type}/{id}"), Route("{type}/identifier/{id}")]
@@ -130,6 +138,7 @@ namespace FhirStarter.Flare.STU3.Controllers
         public HttpResponseMessage Create(string type, Resource resource)
         {
             var service = _handler.FindServiceFromList(_fhirServices, _fhirMockupServices, type);
+            
             resource = (Resource) ValidateResource(resource);
             if (resource is OperationOutcome)
             {
@@ -165,8 +174,11 @@ namespace FhirStarter.Flare.STU3.Controllers
             var headers = Request.Headers;
             var accept = headers.Accept;
             var returnJson = ReturnJson(accept);
-
-            resource = ValidateResource((Resource) resource);
+            if (!(resource is OperationOutcome))
+            {
+                resource = ValidateResource((Resource)resource);
+            }
+            
 
             StringContent httpContent;
             if (!returnJson)
@@ -189,12 +201,23 @@ namespace FhirStarter.Flare.STU3.Controllers
         private Base ValidateResource(Resource resource)
         {
             if (_profileValidator == null) return resource;
+            if (resource is OperationOutcome) return resource;
+            var resourceName = resource.TypeName;
+            var structureDefinition = Load(resourceName);
+            var found = resource.Meta != null && resource.Meta.ProfileElement.Count == 1 &&
+                        resource.Meta.ProfileElement[0].Value.Equals(structureDefinition.Url);
+            if (!found)
+            {
+                throw new ArgumentException($"Profile for {resourceName} must be set to: {structureDefinition.Url}");
+            }
             var resourceAsXDocument = XDocument.Parse(FhirSerializer.SerializeToXml(resource));
             var validationResult = _profileValidator.Validate(resourceAsXDocument.CreateReader(), true);
             if (validationResult.Issue.Count > 0)
             {
                 resource = validationResult;
             }
+
+
             return resource;
         }
 
