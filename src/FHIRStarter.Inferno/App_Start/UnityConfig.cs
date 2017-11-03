a6a590cb4e1c21e55fc10096599c0a81c47826f7
hha046 <harald.hanssen@hnikt.no>
2017-11-03 11:43:15 +0100

    Had to revert to Unity 4.0.1 since the unity.webapi package is still dependent on it.

--- src/FHIRStarter.Inferno/App_Start/UnityConfig.cs
+++ src/FHIRStarter.Inferno/App_Start/UnityConfig.cs
@@ -1,5 +1,5 @@
-using Microsoft.Practices.Unity;
 using System.Web.Http;
+using Microsoft.Practices.Unity;
 using Unity.WebApi;
 
 namespace FhirStarter.Inferno
@@ -9,12 +9,13 @@ namespace FhirStarter.Inferno
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
+
             // e.g. container.RegisterType<ITestService, TestService>();
-            
+
+            //GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
